d1129f07a912658cbb0a43ec8ea4607d8f33a156
Richard Karat <94640733+karatr@users.noreply.github.com>
2023-10-29 15:48:14 +0200

    Delete UFTOneAutomationTests/DigitalLab/DigitalLabSetConficuration directory

--- UFTOneAutomationTests/DigitalLab/DigitalLabSetConficuration/Res1/Report/test_data_src.js
+++
@@ -1,1 +0,0 @@
-﻿var __testDataSource={items:[{dataFilePath:'Default.xlsx',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\Administrator\\Desktop\\tool\\DigitalLabSetConficuration\\Res1',namePairs:[]}]};
