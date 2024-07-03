8f6ff8c6abde7ae532a161bc96888dcb2a28640c
Richard Karat <karat@microfocus.com>
2024-07-03 18:34:18 +0300

    update selenium project

--- SeleniumProject/OpemText-samples/src/main/java/opentext/mbt/utils/Strings.java
+++
@@ -1,8 +0,0 @@
-package opentext.mbt.utils;
-
-public class Strings {
-
-    public static final String url = "https://advantageonlinebanking.com/";
-    public static final String appName = "Advantage Online Banking";
-
-}
