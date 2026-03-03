bc6d6ccb57b14071535c82f56617e8c5672550d3
mbtuser <94625165+mbtuser@users.noreply.github.com>
2026-03-03 14:39:07 +0200

    hhh

---
+++ UFTOneAutomationTests/BrowsersTesChrome/Res2/Report/test_data_src.js
@@ -0,0 +1,1 @@
+﻿var __testDataSource={items:[{dataFilePath:'Default.xlsx',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\rkarat\\OneDrive - OpenText\\Documents\\GitHub\\mbtuser\\UFTOneAutomationTests\\BrowsersTesChrome\\Res2',namePairs:[]}]};
